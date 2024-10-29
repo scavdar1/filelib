04598de1c1ae6372655ea1d396760752457c07ab
scavdar1 <seymacavdar992@gmail.com>
2024-10-29 13:33:50 +0300

    used setuptools package and established a setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="filelib",
+    version="0.1",
+    packages=find_packages(),
+    description='A simple file listing library',
+    author="seyma",
+    author_email="seymacavdar992@gmail.com",
+    url="https://github.com/scavdar1/filelib.git"
+)
+
