706a01ddd0774745d8532a7c72c2c2f95b77b955
scavdar1 <seymacavdar992@gmail.com>
2024-11-03 13:27:44 +0300

    returned none

--- filelib/file_utils.py
+++ filelib/file_utils.py
@@ -6,8 +6,10 @@ def get_files(directory=None, extensions=None):
         directory = os.path.expanduser("~")
     if extensions is None:
         extensions = [".jpg", ".txt"]
-
+    matching_files = []
     for root, dirs, files in os.walk(directory, topdown=True):
         for file in files:
             if any(file.endswith(ext) for ext in extensions):
-                print(os.path.join(root, file))
+                matching_files.append(os.path.join(root, file))
+
+    return matching_files
